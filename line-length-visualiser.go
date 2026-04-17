fc43c6e75d327d87d09e310d2c262f4e336d79f8
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-04-17 08:29:37 +0100

    Refactor to introduce readLines

--- line-length-visualiser.go
+++ line-length-visualiser.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fileName := os.Args[1]
 
+	lines := readLines(fileName)
+	fmt.Println(lines)
+}
+
+func readLines(fileName string) []string {
 	contents := readFile(fileName)
-	fmt.Println(contents)
+
+	lines := strings.Split(contents, "\n")
+
+	return lines
 }
 
 func readFile(fileName string) string {
