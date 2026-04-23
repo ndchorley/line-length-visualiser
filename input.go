87a64b061a4ec29fca76fe7a6955fd926b87f2aa
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-04-23 07:44:41 +0100

    Move file and line reading code to a separate file

---
+++ input.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+func readLines(fileName string) []string {
+	contents := readFile(fileName)
+
+	lines := strings.Split(contents, "\n")
+
+	return lines[0 : len(lines)-1]
+}
+
+func readFile(fileName string) string {
+	bytes, _ := os.ReadFile(fileName)
+	contents := string(bytes)
+
+	return contents
+}
--- line-length-visualiser.go
+++ line-length-visualiser.go
@@ -42,21 +42,6 @@ func makeVisualisation(lengths []int) string {
 	return visualisation.String()
 }
 
-func readLines(fileName string) []string {
-	contents := readFile(fileName)
-
-	lines := strings.Split(contents, "\n")
-
-	return lines[0 : len(lines)-1]
-}
-
-func readFile(fileName string) string {
-	bytes, _ := os.ReadFile(fileName)
-	contents := string(bytes)
-
-	return contents
-}
-
 func toLengths(lines []string) []int {
 	lengths := make([]int, len(lines))
 
